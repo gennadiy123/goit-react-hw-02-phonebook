d1cc27537fa2dc9e07fd47d0fb1d11839f816df0
gennadiy123 <shelukhin_g@ukr.net>
2020-03-15 21:41:24 +0200

    homework

---
+++ build/precache-manifest.fa35cd798b28a5a3790bc818e4a48172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376b345dc7303f75269ff9b786d8e293",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "16ac0115beaed1853558",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ee93c9da.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ee93c9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca20495de35cb7ebdb7",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.30fa4d32.chunk.js"
+  },
+  {
+    "revision": "d36ea81fe01d4d892e84",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.5d1b20ab.js"
+  }
+]);
